0ad4e4186081e3aabc38b66668454e5826f9ac7c
DaveVED <dave.dennis@gs.com>
2025-01-13 21:45:57 -0600

    feat(bootstrap): setup repo and a basic exmaple.

---
+++ scripts/build.ts
@@ -0,0 +1,24 @@
+import { Glob, $ } from "bun";
+
+await $`rm -rf dist`;
+
+const files = new Glob("./src/**/*.{ts,tsx}").scan() as AsyncIterable<string>;
+const collectedFiles: string[] = [];
+for await (const file of files) {
+  collectedFiles.push(file);
+}
+
+await Bun.build({
+  format: "esm",
+  outdir: "dist/esm",
+  external: ["*"],
+  root: "src",
+  /**
+   * My understanding is it's better to pass all the files in at once
+   * instead of building each file individually.
+   */
+  entrypoints: collectedFiles,
+});
+
+// Generate type declarations
+await $`tsc --outDir dist/types --declaration --emitDeclarationOnly --declarationMap`;
